ebb4e02b9eb059e7ad7f8323733e1f74e4d91d3f
callistakrebs <krebs.146@osu.edu>
2020-02-29 23:19:26 -0500

    home screen with navigation functionality

--- screens/QuickLog.tsx
+++ screens/QuickLog.tsx
@@ -1,5 +1,38 @@
-function QuickLog() {
-  return null;
-}
+import React from "react";
+import { Text, View, StyleSheet, ScrollView } from "react-native";
+
+const QuickLog = () => {
+  return (
+    <ScrollView>
+      <View style={styles.questionContainer}>
+        <Text style={styles.textStyle}> How severe was your attack? </Text>
+      </View>
+      <View style={styles.questionContainer}>
+        <Text style={styles.textStyle}> Question 2 </Text>
+      </View>
+      <View style={styles.questionContainer}>
+        <Text style={styles.textStyle}> Question 3 </Text>
+      </View>
+      <View style={styles.questionContainer}>
+        <Text style={styles.textStyle}> Question 4 </Text>
+      </View>
+      <View style={styles.questionContainer}>
+        <Text style={styles.textStyle}> Question 5 </Text>
+      </View>
+    </ScrollView>
+  );
+};
 
 export default QuickLog;
+
+const styles = StyleSheet.create({
+  questionContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  textStyle: {
+    fontSize: 30,
+    padding: 10
+  }
+});
--- screens/SelectScreen.tsx
+++ screens/SelectScreen.tsx
@@ -1,12 +1,56 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React from "react";
+import { Text, View, TouchableOpacity, Alert, StyleSheet } from "react-native";
 
-const SelectScreen = () => {
+const SelectScreen = ({ navigation }) => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }} >
-      <Text>Home! </Text>
-    </View >
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <View style={styles.buttonBlock}>
+        <TouchableOpacity
+          style={styles.buttons}
+          onPress={() => navigation.navigate("Quick Log")}
+        >
+          <Text style={styles.buttonText}> Quick Update </Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.buttonBlock}>
+        <TouchableOpacity
+          style={styles.buttons}
+          onPress={() => navigation.navigate("Detailed Log")}
+        >
+          <Text style={styles.buttonText}> Detailed Update </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 };
 
 export default SelectScreen;
+
+const styles = StyleSheet.create({
+  buttonBlock: {
+    height: "40%",
+    width: "80%",
+    padding: 10,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  buttons: {
+    padding: 20,
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+    backgroundColor: "white",
+    borderRadius: 10,
+    justifyContent: "center",
+    shadowColor: "#000000",
+    shadowOpacity: 0.3,
+    shadowRadius: 2,
+    shadowOffset: {
+      height: 1,
+      width: 1
+    }
+  },
+  buttonText: {
+    fontSize: 35
+  }
+});
