f3f17fc35591276eb816c2ab676d3ed49c6f2c15
Garrett Morse <morse.172@osu.edu>
2020-03-01 00:43:59 -0500

    bro i swear just one more commit bro i swear, i forgot my git repo at home bro i swear just one commit and im done for the night

--- screens/QuickLog.tsx
+++ screens/QuickLog.tsx
@@ -1,37 +1,42 @@
 import React from "react";
-import { Text, View, StyleSheet, ScrollView, Slider } from "react-native";
+import { Text, View, StyleSheet, ScrollView } from "react-native";
+import Slider from 'react-native-slider';
 import styles from "../util/styles";
 
-class QuickLog extends React.Component {
+class QuickLog extends React.Component<{}, { value: number }> {
+  sliderTimeoutId: NodeJS.Timeout;
   constructor(props) {
     super(props);
-    this.state = { value: 5 };
+    this.state = { value: 1 };
   }
+
   render() {
     return (
       <ScrollView>
-        <View style={screenStyles.questionContainer}>
+        <View style={styles.questionContainer}>
           <Text style={styles.text}> How severe was your attack? </Text>
           <Slider
             style={styles.slider}
-            maximumValue={10}
+            maximumValue={5}
             minimumValue={1}
-            value={5}
+            value={1}
             step={1}
-            onValueChange={value => this.setState({ value })}
+            onValueChange={value => {
+              this.setState({ value })
+            }}
           ></Slider>
           <Text style={styles.text}>{this.state.value}</Text>
         </View>
-        <View style={screenStyles.questionContainer}>
+        <View style={styles.questionContainer}>
           <Text style={styles.text}> Question 2 </Text>
         </View>
-        <View style={screenStyles.questionContainer}>
+        <View style={styles.questionContainer}>
           <Text style={styles.text}> Question 3 </Text>
         </View>
-        <View style={screenStyles.questionContainer}>
+        <View style={styles.questionContainer}>
           <Text style={styles.text}> Question 4 </Text>
         </View>
-        <View style={screenStyles.questionContainer}>
+        <View style={styles.questionContainer}>
           <Text style={styles.text}> Question 5 </Text>
         </View>
       </ScrollView>
@@ -40,11 +45,3 @@ class QuickLog extends React.Component {
 }
 
 export default QuickLog;
-
-const screenStyles = StyleSheet.create({
-  questionContainer: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
--- util/styles.tsx
+++ util/styles.tsx
@@ -1,31 +1,36 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    paddingHorizontal: 10
-  },
-  button: {
-    alignItems: "center",
-    backgroundColor: "skyblue",
-    padding: 50,
-    width: "90%",
-    marginTop: 50,
-    marginBottom: 50,
-    borderRadius: 100,
-    shadowRadius: 1,
-    shadowColor: "black",
-    shadowOffset: { width: -2, height: 2 },
-    shadowOpacity: 1
-  },
-  text: {
-    fontSize: 25,
-    color: "darkblue"
-  },
-  slider: {
-    width: "80%"
-  }
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        paddingHorizontal: 10
+    },
+    button: {
+        alignItems: "center",
+        backgroundColor: "skyblue",
+        padding: 50,
+        width: "90%",
+        marginTop: 50,
+        marginBottom: 50,
+        borderRadius: 100,
+        shadowRadius: 1,
+        shadowColor: "black",
+        shadowOffset: { width: -2, height: 2 },
+        shadowOpacity: 1
+    },
+    text: {
+        fontSize: 25,
+        color: "darkblue"
+    },
+    slider: {
+        width: "80%"
+    },
+    questionContainer: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center"
+    }
 });
 
 export default styles;
