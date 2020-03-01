d10f5f48dc12329eb17c505c8b5ca318c279ee94
callistakrebs <krebs.146@osu.edu>
2020-03-01 00:03:09 -0500

    garrett appreciating me for the first time in my life

--- screens/QuickLog.tsx
+++ screens/QuickLog.tsx
@@ -2,34 +2,42 @@ import React from "react";
 import { Text, View, StyleSheet, ScrollView, Slider } from "react-native";
 import styles from "../util/styles";
 
-const QuickLog = () => {
-  return (
-    <ScrollView>
-      <View style={screenStyles.questionContainer}>
-        <Text style={styles.text}> How severe was your attack? </Text>
-        <Slider
-          style={styles.slider}
-          maximumValue={10}
-          minimumValue={1}
-          value={5}
-          step={1}
-        ></Slider>
-      </View>
-      <View style={screenStyles.questionContainer}>
-        <Text style={styles.text}> Question 2 </Text>
-      </View>
-      <View style={screenStyles.questionContainer}>
-        <Text style={styles.text}> Question 3 </Text>
-      </View>
-      <View style={screenStyles.questionContainer}>
-        <Text style={styles.text}> Question 4 </Text>
-      </View>
-      <View style={screenStyles.questionContainer}>
-        <Text style={styles.text}> Question 5 </Text>
-      </View>
-    </ScrollView>
-  );
-};
+class QuickLog extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: 5 };
+  }
+  render() {
+    return (
+      <ScrollView>
+        <View style={screenStyles.questionContainer}>
+          <Text style={styles.text}> How severe was your attack? </Text>
+          <Slider
+            style={styles.slider}
+            maximumValue={10}
+            minimumValue={1}
+            value={5}
+            step={1}
+            onValueChange={value => this.setState({ value })}
+          ></Slider>
+          <Text style={styles.text}>{this.state.value}</Text>
+        </View>
+        <View style={screenStyles.questionContainer}>
+          <Text style={styles.text}> Question 2 </Text>
+        </View>
+        <View style={screenStyles.questionContainer}>
+          <Text style={styles.text}> Question 3 </Text>
+        </View>
+        <View style={screenStyles.questionContainer}>
+          <Text style={styles.text}> Question 4 </Text>
+        </View>
+        <View style={screenStyles.questionContainer}>
+          <Text style={styles.text}> Question 5 </Text>
+        </View>
+      </ScrollView>
+    );
+  }
+}
 
 export default QuickLog;
 
