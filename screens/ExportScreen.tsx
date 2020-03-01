adb48af99d7c568b27b295237e52d56ade8f23b0
Sam Stevens <stevens.994@osu.edu>
2020-02-29 23:46:01 -0500

    I spar with zuckerberg himself

--- screens/ExportScreen.tsx
+++ screens/ExportScreen.tsx
@@ -5,9 +5,35 @@ import { LogContext } from '../context/Log';
 
 
 
-function ExportCard() {
-  return null;
+function ExportCard({ displayName }) {
+  return (
+    <View style={{
+      flex: 1, margin: 15,
+      padding: 30,
+      alignItems: 'center',
+      backgroundColor: 'skyblue',
+      shadowColor: "black",
+      shadowOffset: { width: -2, height: 2 },
+      shadowOpacity: 0.7
+    }} key={displayName}>
+      <Text style={{ color: 'darkblue', fontSize: 26 }}>{displayName}</Text>
+    </View>
+  );
 }
+
+
+
+interface ExportType {
+  extension: string,
+  displayName: string,
+}
+
+const SupportedExportTypes: ExportType[] = [
+  { extension: 'xlsx', displayName: 'Excel' },
+  { extension: 'csv', displayName: 'CSV' },
+  { extension: 'json', displayName: 'JSON' },
+  { extension: 'txt', displayName: 'Text' }
+];
 
 export default class ExportScreen extends Component<{ navigation, screenProps; }, {}> {
   constructor(props) {
@@ -28,6 +54,7 @@ export default class ExportScreen extends Component<{ navigation, screenProps; }
                 <Text>{log.time.toDateString()}</Text>
               </View>)}
           </LogContext.Consumer>
+          {SupportedExportTypes.map(exportType => ExportCard({ displayName: exportType.displayName }))}
         </ScrollView>
         <LogContext.Consumer>
           {context =>
