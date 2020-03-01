3b1386d0f4040643dcab6b5b7f886e2791c26198
Garrett Morse <morse.172@osu.edu>
2020-02-29 23:18:21 -0500

    add styling :)

--- screens/SelectScreen.tsx
+++ screens/SelectScreen.tsx
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native';
 const SelectScreen = () => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }} >
-      <Text>Home! </Text>
+      <Text>Home!</Text>
     </View >
   );
 };
---
+++ util/drawButton.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+import { callNumber } from './phonecall';
+import styles from "./styles";
+
+function drawButton(name: string, telNumber: string) {
+    return (
+        <TouchableOpacity onPress={() => callNumber(telNumber)} style={styles.button} >
+            <Text style={styles.text}>{name}</Text>
+        </TouchableOpacity>
+    )
+}
+export default drawButton;
